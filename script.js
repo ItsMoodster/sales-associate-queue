5ae81892c0fc02474d526ac9bce846919f368ec6
ItsMoodster <128945595+ItsMoodster@users.noreply.github.com>
2023-03-26 13:16:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,22 @@
-body {
-  font-family: Arial, sans-serif;
-  max-width: 600px;
-  margin: 0 auto;
-  padding: 20px;
-}
+document.getElementById("addName").addEventListener("click", function() {
+    const name = document.getElementById("nameInput").value;
+    if (name) {
+        addToList(name, "waitingQueue");
+    }
+    document.getElementById("nameInput").value = "";
+});
 
-h1 {
-  text-align: center;
-  margin-bottom: 40px;
-}
-
-form {
-  display: flex;
-  justify-content: center;
-  margin-bottom: 20px;
-}
-
-input[type="text"] {
-  width: 100%;
-  margin-right: 10px;
-  padding: 5px;
-}
-
-button {
-  padding: 5px;
-}
-
-.queue-container {
-  display: flex;
-  justify-content: space-between;
-  align-items: flex-start;
-  margin-bottom: 20px;
-}
-
-.queue-box, .with-customer-box {
-  flex-grow: 1;
-  padding: 20px;
-  border: 1px solid #ccc;
-}
-
-.queue-box:last-child, .with-customer-box:last-child {
-  margin-right: 0;
-}
-
-.queue-item {
-  color: green;
-}
-
-.with-customer {
-  color: red;
+function addToList(name, listId) {
+    const list = document.getElementById(listId);
+    const listItem = document.createElement("li");
+    listItem.innerText = name;
+    listItem.addEventListener("click", function() {
+        if (listId === "waitingQueue") {
+            addToList(name, "currentlyWith");
+        } else {
+            addToList(name, "waitingQueue");
+        }
+        list.removeChild(listItem);
+    });
+    list.appendChild(listItem);
 }
